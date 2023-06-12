131606b6709c10c740788f8fd45faff1afe3dfc9
Luis <luis@staudt-fridingen.de>
2023-06-12 20:18:40 +0200

    added a missing ChangeListener

--- src/main/java/net/luis/fxutils/Decorators.java
+++ src/main/java/net/luis/fxutils/Decorators.java
@@ -2,6 +2,7 @@ package net.luis.fxutils;
 
 import org.controlsfx.control.decoration.Decorator;
 import org.controlsfx.control.decoration.GraphicDecoration;
+import org.jetbrains.annotations.NotNull;
 
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -18,40 +19,48 @@ public class Decorators {
 	
 	public static final ImageView INFO_DECORATION = new ImageView(new Image(Decorators.class.getResource("/impl/org/controlsfx/control/validation/decoration-info.png").toExternalForm()));
 	
+	@NotNull
 	public static void addGraphicInfoDecoration(Node node, Pos pos) {
 		Decorator.addDecoration(node, new GraphicDecoration(INFO_DECORATION, pos));
 	}
 	
+	@NotNull
 	public static void addGraphicInfoDecoration(Node node, Pos pos, double xOffset, double yOffset) {
 		Decorator.addDecoration(node, new GraphicDecoration(INFO_DECORATION, pos, xOffset, yOffset));
 	}
 	
 	public static final ImageView WARNING_DECORATION = new ImageView(new Image(Decorators.class.getResource("/impl/org/controlsfx/control/validation/decoration-warning.png").toExternalForm()));
 	
+	@NotNull
 	public static void addGraphicWarningDecoration(Node node, Pos pos) {
 		Decorator.addDecoration(node, new GraphicDecoration(WARNING_DECORATION, pos));
 	}
 	
+	@NotNull
 	public static void addGraphicWarningDecoration(Node node, Pos pos, double xOffset, double yOffset) {
 		Decorator.addDecoration(node, new GraphicDecoration(WARNING_DECORATION, pos, xOffset, yOffset));
 	}
 	
 	public static final ImageView ERROR_DECORATION = new ImageView(new Image(Decorators.class.getResource("/impl/org/controlsfx/control/validation/decoration-error.png").toExternalForm()));
 	
+	@NotNull
 	public static void addGraphicErrorDecoration(Node node, Pos pos) {
 		Decorator.addDecoration(node, new GraphicDecoration(ERROR_DECORATION, pos));
 	}
 	
+	@NotNull
 	public static void addGraphicErrorDecoration(Node node, Pos pos, double xOffset, double yOffset) {
 		Decorator.addDecoration(node, new GraphicDecoration(ERROR_DECORATION, pos, xOffset, yOffset));
 	}
 	
 	public static final ImageView REQUIRED_DECORATION = new ImageView(new Image(Decorators.class.getResource("/impl/org/controlsfx/control/validation/required-indicator.png").toExternalForm()));
 	
+	@NotNull
 	public static void addGraphicRequiredDecoration(Node node, Pos pos) {
 		Decorator.addDecoration(node, new GraphicDecoration(REQUIRED_DECORATION, pos));
 	}
 	
+	@NotNull
 	public static void addGraphicRequiredDecoration(Node node, Pos pos, double xOffset, double yOffset) {
 		Decorator.addDecoration(node, new GraphicDecoration(REQUIRED_DECORATION, pos, xOffset, yOffset));
 	}
--- src/main/java/net/luis/fxutils/PropertyListeners.java
+++ src/main/java/net/luis/fxutils/PropertyListeners.java
@@ -1,7 +1,10 @@
 package net.luis.fxutils;
 
+import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+
+import org.jetbrains.annotations.NotNull;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -14,46 +17,61 @@ import javafx.beans.value.ObservableValue;
 
 public class PropertyListeners {
 	
-	public static <T> ChangeListener<T> create(BiPredicate<T, T> listenerPredicate, Runnable listenerRunnable) {
+	@NotNull
+	public static <T> ChangeListener<T> create(BiConsumer<T, T> listenerAction) {
 		return new ChangeListener<T>() {
 			@Override
 			public void changed(ObservableValue<? extends T> observable, T oldValue, T newValue) {
-				if (listenerPredicate.test(oldValue, newValue)) {
-					listenerRunnable.run();
+				listenerAction.accept(oldValue, newValue);
+			}
+		};
+	}
+	
+	@NotNull
+	public static <T> ChangeListener<T> create(BiPredicate<T, T> listenerCondition, Runnable listenerAction) {
+		return new ChangeListener<T>() {
+			@Override
+			public void changed(ObservableValue<? extends T> observable, T oldValue, T newValue) {
+				if (listenerCondition.test(oldValue, newValue)) {
+					listenerAction.run();
 				}
 			}
 		};
 	}
 	
-	public static <T> ChangeListener<T> createWithOld(T conditionValue, Runnable listenerRunnable) {
+	@NotNull
+	public static <T> ChangeListener<T> createWithOld(T conditionValue, Runnable listenerAction) {
 		return createWithOld((value) -> {
 			return value.equals(conditionValue);
-		}, listenerRunnable);
+		}, listenerAction);
 	}
 	
-	public static <T> ChangeListener<T> createWithOld(Predicate<T> listenerPredicate, Runnable listenerRunnable) {
+	@NotNull
+	public static <T> ChangeListener<T> createWithOld(Predicate<T> listenerCondition, Runnable listenerAction) {
 		return new ChangeListener<T>() {
 			@Override
 			public void changed(ObservableValue<? extends T> observable, T oldValue, T newValue) {
-				if (listenerPredicate.test(oldValue)) {
-					listenerRunnable.run();
+				if (listenerCondition.test(oldValue)) {
+					listenerAction.run();
 				}
 			}
 		};
 	}
 	
-	public static <T> ChangeListener<T> createWithNew(T conditionValue, Runnable listenerRunnable) {
+	@NotNull
+	public static <T> ChangeListener<T> createWithNew(T conditionValue, Runnable listenerAction) {
 		return createWithNew((value) -> {
 			return value.equals(conditionValue);
-		}, listenerRunnable);
+		}, listenerAction);
 	}
 	
-	public static <T> ChangeListener<T> createWithNew(Predicate<T> listenerPredicate, Runnable listenerRunnable) {
+	@NotNull
+	public static <T> ChangeListener<T> createWithNew(Predicate<T> listenerCondition, Runnable listenerAction) {
 		return new ChangeListener<T>() {
 			@Override
 			public void changed(ObservableValue<? extends T> observable, T oldValue, T newValue) {
-				if (listenerPredicate.test(newValue)) {
-					listenerRunnable.run();
+				if (listenerCondition.test(newValue)) {
+					listenerAction.run();
 				}
 			}
 		};
