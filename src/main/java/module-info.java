a3ce2478df21fd85727b358bbc3aa1367473154c
Luis <luis@staudt-fridingen.de>
2023-10-07 21:06:41 +0200

    added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+module net.luis.fxutils {
+	requires javafx.base;
+	requires org.jetbrains.annotations;
+	requires javafx.graphics;
+	requires javafx.controls;
+}
